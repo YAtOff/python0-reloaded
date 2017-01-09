b110308f1ed9c65f0f88a9dd6ce017d1290c5d76
Yavor Atov <yavor.atov@gmail.com>
2017-01-09 21:47:52 +0200

    Added bonus task for week 5

---
+++ week5/bonus-homework/validdate.py
@@ -0,0 +1,19 @@
+"""
+Напишете функция `date_is_valid`,
+която приема дата под формата на: година, месец, ден, час, минута, секунда;
+и връща `True` ако датата е влидна и `False` в противен случай.
+
+```python
+>>> data_is_valid(2015, 1, 2, 23, 20, 10)
+True
+>>> data_is_valid(2015, 2, 29, 12, 10, 10)
+False
+>>> data_is_valid(2012, 2, 29, 12, 10, 10)
+True
+>>> data_is_valid(2015, 11, 31, 13, 30, 50)
+False
+```
+"""
+
+def data_is_valid(year, month, day, hour, minute, second):
+    pass
