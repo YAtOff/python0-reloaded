965e33981bc8bf7ba27c0c597731d0c315334551
Yavor Atov <yavor.atov@gmail.com>
2017-03-05 23:25:44 +0200

    Added tasks for week 12

---
+++ week12/tasks/for_traverse.py
@@ -0,0 +1,31 @@
+books = ["Learn You a Haskell",
+        "The Healthy Programmer",
+        "Code Complete",
+        "The Pragmatic Programmer",
+        "Pro Git",
+        "Introduction to Algorithms",
+        "Concrete Mathematics"]
+
+
+def for_traverse():
+    """
+    books = ["Learn You a Haskell",
+            "The Healthy Programmer",
+            "Code Complete",
+            "The Pragmatic Programmer",
+            "Pro Git",
+            "Introduction to Algorithms",
+            "Concrete Mathematics"]
+
+    * Обхожданията трябва да отпечатат на екрана името на всяка книга на нов ред.
+
+    >>> for_traverse()
+    Learn You a Haskell
+    The Healthy Programmer
+    Code Complete
+    The Pragmatic Programmer
+    Pro Git
+    Introduction to Algorithms
+    Concrete Mathematics
+    """
+    pass
---
+++ week12/tasks/tests.py
@@ -0,0 +1,29 @@
+import doctest
+
+import for_traverse
+import while_traverse
+import evens_count
+import sum_numbers
+import max_number
+import min_number
+import words_count
+import head
+import tail
+import last
+import take
+import drop
+import reverse
+
+doctest.testmod(for_traverse)
+doctest.testmod(while_traverse)
+doctest.testmod(evens_count)
+doctest.testmod(sum_numbers)
+doctest.testmod(max_number)
+doctest.testmod(min_number)
+doctest.testmod(words_count)
+doctest.testmod(head)
+doctest.testmod(tail)
+doctest.testmod(last)
+doctest.testmod(take)
+doctest.testmod(drop)
+doctest.testmod(reverse)
