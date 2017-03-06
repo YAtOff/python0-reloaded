17404d8a19e7d46033012c17403d6c069efb56cf
Yavor Atov <yavor.atov@gmail.com>
2017-03-06 09:04:34 +0200

    Added tasks for week 13

--- week12/tasks/tests.py
+++ week12/tasks/tests.py
@@ -8,11 +8,7 @@ import max_number
 import min_number
 import words_count
 import head
-import tail
 import last
-import take
-import drop
-import reverse
 
 doctest.testmod(for_traverse)
 doctest.testmod(while_traverse)
@@ -22,8 +18,4 @@ doctest.testmod(max_number)
 doctest.testmod(min_number)
 doctest.testmod(words_count)
 doctest.testmod(head)
-doctest.testmod(tail)
 doctest.testmod(last)
-doctest.testmod(take)
-doctest.testmod(drop)
-doctest.testmod(reverse)
---
+++ week13/tasks/tests.py
@@ -0,0 +1,11 @@
+import doctest
+
+import tail
+import take
+import drop
+import reverse
+
+doctest.testmod(tail)
+doctest.testmod(take)
+doctest.testmod(drop)
+doctest.testmod(reverse)
